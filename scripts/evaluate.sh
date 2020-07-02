933a19a11d5f70d27caf06ad7de23beea8769634
Turab Iqbal <t.iqbal@surrey.ac.uk>
2020-07-02 12:00:07 +0100

    Fix error in evaluate.sh script

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -5,7 +5,7 @@ set -e
 evaluate() {
   echo "Evaluating '$1'..."
   python task5/extern/evaluate_predictions.py \
-    "_workspace/$1/seed=1000/validation.csv" \
+    "_workspace/predictions/$1/seed=1000/validation.csv" \
     '_dataset/annotations.csv' \
     '_dataset/dcase-ust-taxonomy.yaml'
   echo
